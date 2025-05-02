980a3c698358f02ba25fbbdd78298b72e9808362
a <abdulrhmanabotor@gmail.com>
2025-05-02 18:31:38 +0300

    chore: setup & eventhandler

--- src/main/java/com/Podzilla/analytics/messaging/RabbitListener.java
+++ src/main/java/com/Podzilla/analytics/messaging/RabbitListener.java
@@ -1,5 +1,11 @@
 package com.Podzilla.analytics.messaging;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.Podzilla.analytics.eventhandler.EventHandlerDispatcher;
+
 public class RabbitListener {
     
+    @Autowired
+    EventHandlerDispatcher dispatcher;
 }
